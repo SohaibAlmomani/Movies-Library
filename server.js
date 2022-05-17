fe5d7c1c763e564fbb91e6ff774e6902e87d0711
SohaibAlmomani <almomanisohaib95@yahoo.com>
2022-05-18 01:20:01 +0300

    Movies-Library

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ dotenv.config();
 // end point
 app.get("/", homeHandler);
 app.get("/favorite", favoriteHandler);
+app.get("/trending", trendingPageHandler);
+app.post("/addMovie", addMovieHandler);
+app.get("/getMovie/:id", getMovieByIdHandler);
 app.get("*", errorHandler);
 
 // Constructor
@@ -57,10 +60,51 @@ function errorHandler(req, res) {
     return res.status(500).send("Error : page not found");
 }
 
+function trendingPageHandler(req, res) {
+    const apiUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}`;
+    axios;
+    axios
+        .get(apiUrl)
+        .then((value) => {
+            let trendingMovies = value.data.results.map((movie) => {
+                return new MoviesLibrary(movie.id, movie.title, movie.releaseDate, movie.posterPath, movie.overview);
+            });
+            return res.status(200).json(trendingMovies);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.send("Inside catch");
+        });
+}
+
+function addMovieHandler(req, res) {
+    let { name, time, summary, image, comment } = req.body;
+
+    let sql = "INSERT INTO movie(name,time,summary,image,comment) VALUES($1, $2, $3, $4, $5) RETURNING *;";
+    let values = [name, time, summary, image, comment];
+    client
+        .query(sql, values)
+        .then((result) => {
+            return res.status(201).json(result.rows[0]);
+        })
+        .catch();
+}
+
+function getMovieByIdHandler(req, res) {
+    let id = req.params.id;
+    let sql = `SELECT * FROM movie WHERE id=${id};`;
+
+    client
+        .query(sql)
+        .then((result) => {
+            res.status(200).json(result.rows);
+        })
+        .catch();
+}
+
 // after connection to db, start the server
 client.connect().then(() => {
     app.listen(port, () => {
         console.log(`Server is listening ${port}`);
     });
 });
-
